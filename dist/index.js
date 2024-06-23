6b4534890a3fe39a99fb73ff2a35b345211b4cc0
22sook00 <22sook00@gmail.com>
2024-06-23 16:37:26 +0900

    🐛 Fix: package.json main changed

--- dist/index.js
+++ dist/index.js
@@ -14,3 +14,25 @@ Object.keys(_util).forEach(function (key) {
     }
   });
 });
+var _util2 = require("./utils/util.date");
+Object.keys(_util2).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _util2[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _util2[key];
+    }
+  });
+});
+var _formatUtil = require("./utils/formatUtil");
+Object.keys(_formatUtil).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (key in exports && exports[key] === _formatUtil[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _formatUtil[key];
+    }
+  });
+});
